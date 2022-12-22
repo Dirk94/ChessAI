4a9cac7c0aec6ab50e75738cfe8ad8dd28a09436
ox1-omnidex <aglock17inmybag@proton.me>
2022-12-22 13:54:55 +0100

    Upgrade for the get_user_move() and letter_to_xpos() function

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import board, pieces, ai
 
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
 # Returns a move object based on the users input. Does not check if the move is valid.
 def get_user_move():
     print("Example Move: A2 A4")
-    move_str = input("Your Move: ")
-    move_str = move_str.replace(" ", "")
+    player_move = input("Your Move: ").split()
 
     try:
-        xfrom = letter_to_xpos(move_str[0:1])
-        yfrom = 8 - int(move_str[1:2]) # The board is drawn "upside down", so flip the y coordinate.
-        xto = letter_to_xpos(move_str[2:3])
-        yto = 8 - int(move_str[3:4]) # The board is drawn "upside down", so flip the y coordinate.
+        _from = player_move[0]
+        _to = player_move[1]
+        
+        xfrom = letter_to_xpos(_from[0])
+        yfrom = 8 - int(_from[1]) # The board is drawn "upside down", so flip the y coordinate.
+        xto = letter_to_xpos(_to[0])
+        yto = 8 - int(_to[1]) # The board is drawn "upside down", so flip the y coordinate.
+        
         return ai.Move(xfrom, yfrom, xto, yto, False)
+
     except ValueError:
         print("Invalid format. Example: A2 A4")
         return get_user_move()
@@ -38,27 +44,8 @@ def get_valid_user_move(board):
             print("Invalid move.")
     return move
 
-# Converts a letter (A-H) to the x position on the chess board.
 def letter_to_xpos(letter):
-    letter = letter.upper()
-    if letter == 'A':
-        return 0
-    if letter == 'B':
-        return 1
-    if letter == 'C':
-        return 2
-    if letter == 'D':
-        return 3
-    if letter == 'E':
-        return 4
-    if letter == 'F':
-        return 5
-    if letter == 'G':
-        return 6
-    if letter == 'H':
-        return 7
-
-    raise ValueError("Invalid letter.")
+    return chars.index(letter)
 
 #
 # Entry point.
